2b252f60e8c284e90fa8182926a346541103b6d7
Karthik G. Appiah <kar@kar-m1pro.local>
2026-09-16 13:15:40 -0700

    feat: provision NAT gateway

--- gateways.tf
+++ gateways.tf
@@ -9,3 +9,20 @@ output "internet_gateway_id" {
   description = "OCID of the internet gateway used by the public subnet"
   value       = oci_core_internet_gateway.main.id
 }
+
+resource "oci_core_nat_gateway" "main" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.main.id
+  display_name   = "main-nat-gateway"
+  block_traffic  = false
+}
+
+output "nat_gateway_id" {
+  description = "OCID of the NAT gateway used for outbound traffic from the private subnet"
+  value       = oci_core_nat_gateway.main.id
+}
+
+output "nat_gateway_public_ip" {
+  description = "Public IP address that private subnet traffic is translated to"
+  value       = oci_core_nat_gateway.main.nat_ip
+}
