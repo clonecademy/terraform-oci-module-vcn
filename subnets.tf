a977e65d3718aff53cb054652377f410fd3f5ba8
Karthik G. Appiah <kar@kar-m1pro.local>
2026-09-16 13:16:09 -0700

    feat: provision public subnet

---
+++ subnets.tf
@@ -0,0 +1,84 @@
+variable "public_subnet_cidr_block" {
+  description = "IPv4 CIDR block of the public subnet, which is reachable from the internet."
+  type        = string
+  default     = "10.0.0.0/24"
+
+  validation {
+    condition     = can(cidrhost(var.public_subnet_cidr_block, 0))
+    error_message = "Public subnet CIDR block must be valid IPv4 CIDR notation, e.g. 10.0.0.0/24."
+  }
+}
+
+resource "oci_core_default_route_table" "public" {
+  manage_default_resource_id = oci_core_vcn.main.default_route_table_id
+
+  route_rules {
+    destination       = local.anywhere_cidr
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.main.id
+  }
+}
+
+resource "oci_core_default_security_list" "public" {
+  manage_default_resource_id = oci_core_vcn.main.default_security_list_id
+
+  ingress_security_rules {
+    protocol    = local.protocol_tcp
+    source      = local.anywhere_cidr
+    source_type = "CIDR_BLOCK"
+
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }
+
+  ingress_security_rules {
+    protocol    = local.protocol_icmp
+    source      = local.anywhere_cidr
+    source_type = "CIDR_BLOCK"
+
+    icmp_options {
+      type = 3
+      code = 4
+    }
+  }
+
+  dynamic "ingress_security_rules" {
+    for_each = toset(var.vcn_cidr_blocks)
+
+    content {
+      protocol    = local.protocol_icmp
+      source      = ingress_security_rules.value
+      source_type = "CIDR_BLOCK"
+
+      icmp_options {
+        type = 3
+      }
+    }
+  }
+
+  egress_security_rules {
+    protocol         = local.protocol_all
+    destination      = local.anywhere_cidr
+    destination_type = "CIDR_BLOCK"
+  }
+}
+
+resource "oci_core_subnet" "public" {
+  compartment_id             = var.compartment_id
+  vcn_id                     = oci_core_vcn.main.id
+  cidr_block                 = var.public_subnet_cidr_block
+  display_name               = "main-public-subnet"
+  dns_label                  = "public"
+  dhcp_options_id            = oci_core_vcn.main.default_dhcp_options_id
+  route_table_id             = oci_core_default_route_table.public.id
+  security_list_ids          = [oci_core_default_security_list.public.id]
+  prohibit_public_ip_on_vnic = false
+  prohibit_internet_ingress  = false
+}
+
+output "public_subnet_id" {
+  description = "OCID of the public subnet"
+  value       = oci_core_subnet.public.id
+}
